92965beae0160962e42e9557c125bd78afc2b8e6
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-09-15 15:06:54 -0300

    Moved script for sidebar to root. Applied it to project section

---
+++ sidebar-script.js
@@ -0,0 +1,18 @@
+function openNav() {
+if (x.matches) { // If media query matches
+    document.getElementById("sidebar").style.width = "100vw";
+  } else {
+   document.getElementById("sidebar").style.width = "25vw";
+  }
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  // document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+var x = window.matchMedia("(max-width: 1024px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
