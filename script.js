b706372632a53c44a3e8391b1746ee3c184e448d
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-09-14 22:02:18 -0300

    Added responsive to sidebar

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+// function openNav() {
+//   document.getElementById("sidebar").style.width = "25vw";
+//   // document.getElementById("main").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("sidebar").style.width = "0";
+//   // document.getElementById("main").style.marginLeft= "0";
+// }
+
 function openNav() {
-  document.getElementById("sidebar").style.width = "25vw";
-  // document.getElementById("main").style.marginLeft = "250px";
+if (x.matches) { // If media query matches
+    document.getElementById("sidebar").style.width = "100vw";
+  } else {
+   document.getElementById("sidebar").style.width = "25vw";
+  }
 }
 
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
   // document.getElementById("main").style.marginLeft= "0";
 }
+
+
+
+var x = window.matchMedia("(max-width: 1024px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
