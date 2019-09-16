0bfc0cdfbcdb33136a13dfa265b4e882a32f9254
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-09-15 22:49:48 -0300

    changes

--- Projects/CasaFT/sidebar-script.js
+++ Projects/CasaFT/sidebar-script.js
@@ -14,5 +14,5 @@ function closeNav() {
 
 
 var x = window.matchMedia("(max-width: 1024px)")
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
